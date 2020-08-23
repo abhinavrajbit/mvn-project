b11ec7715991b53ec9d122afdd6a78d7e63bb44e
Abhina Raj <abhinavrjbit@gmail.com>
2020-08-23 18:42:04 +0530

    first commit

---
+++ test1/src/main/java/abhinav/test1/App.java
@@ -0,0 +1,39 @@
+package abhinav.test1;
+
+//import org.openqa.selenium.By;
+//import org.testng.*;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
+//import org.testng.Assert;
+//import org.testng.annotations.*;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+//	WebDriver driver;
+//    @Test
+    public void test() {
+//    	System.setProperty("webdriver.chrome.driver", "E:\\Angular 6\\Amdocs Solution\\chromedriver_win32\\chromedriver.exe");
+//    	ChromeOptions options = new ChromeOptions();
+//    	options.addArguments("headless");
+//    	driver = new ChromeDriver(options);
+//    	driver.get("http://www.intellipaat.com");
+//    	driver.findElement(By.xpath("//input[@id='s']")).click();
+//    	driver.findElement(By.xpath("//input[@id='s']")).sendKeys("DevOps");
+//    	driver.findElement(By.xpath("//button[@id='home-search-btn']")).click();
+//    	driver.findElement(By.xpath("//h3[contains(text(),'DevOps Certification Training Course Online')]")).click();
+//    	String expected = "DevOps Certification Training Course Online";
+//    	
+//    	String actual = driver.findElement(By.xpath("//h1[contains(text(),'DevOps Certification Training Course Online')]")).getText();
+//    	Assert.assertEquals(actual, expected);
+    	System.out.println("Test is execution");
+    }
+    
+}
+
+
